3d71532b750d086cd35595a903d06c9ca68c3583
EmanuelScherer <emanuel.scherer@meta.com.br>
2020-04-01 13:39:12 -0300

    .

---
+++ Test/test.ts
@@ -0,0 +1,7 @@
+
+let a: string = "a"
+console.log(a)
+
+//let lib = require('../lib/EMFR.ts')
+
+//EMFR.MoveMouse(1,1)
--- lib/EMFR.ts
+++ lib/EMFR.ts
@@ -1,11 +1,32 @@
+/**
+ * Emanuel Master Functions RPA (EMFR)
+ * 
+ * @author Emanuel de Souza Scherer
+ * 
+ * @module EMFR
+ * 
+ * @requires @types/node^13.9.5
+ * @requires fs@0.0.1-security
+ * @requires jimp^0.9.8
+ * @requires nan^2.14.0
+ * @requires python-bridge^1.1.0
+ * @requires robotjs^0.6.0
+*/
+
+// LIBS
 const robot = require('robotjs')
 const pythonBridge = require('python-bridge')
 const fs = require('fs')
 
+// LETS GERAIS
 const python = pythonBridge()
 
-module.exports = {
+const EMFR = module.exports = {
 
-    Read
+    MoveMouse: (x: number, y: number): void => {
+
+        robot.moveMouse(x,y)
+
+    }
 
 }
